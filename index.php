dfab01f500c231d661f6b1243767b225e42a9679
Ninad Ramade <ninad.ramade@pegasusone.com>
2022-11-15 14:52:33 +0530

    bug fix

--- index.php
+++ index.php
@@ -287,9 +287,8 @@ function getCandidate(email) {
             var candidateData = Object.entries(this.response);
             candidateData.forEach(function(e){
             if(e[0] == 'skills' || e[0] == 'currentLocation' || e[0] == 'preferredLocation') {
-            	var values = e[1].split(", ");
+            	var values = e[1].split(",");
         		values.forEach(function(value){
-            		console.log(value);
         			document.querySelector("#" + e[0] + " option[value='" + value + "']").setAttribute("selected", "selected");
         		});
             } else if(e[0] == 'servingNotice') {
