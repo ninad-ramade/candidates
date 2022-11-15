22b550911efa9cbec5e4ad94feced3b434aad34f
Ninad Ramade <ninad.ramade@pegasusone.com>
2022-11-15 15:48:34 +0530

    candidate changes

--- menu.php
+++ menu.php
@@ -24,4 +24,4 @@ if(empty($email) && $mode != 'new') {
   </div>
 </nav>
 <?php }} ?>
-<div class="container-fluid <?php echo $_SERVER['REQUEST_URI'] == '/' ? 'candidateFormBg' : ''; ?>">
+<div class="container-fluid <?php echo $_SERVER['REQUEST_URI'] == '/' || strstr($_SERVER['REQUEST_URI'], 'index.php') ? 'candidateFormBg' : ''; ?>">
