3113c8e6b70cabcb9751383d5ae4129b7f6b1f76
Ninad Ramade <ninad.ramade@pegasusone.com>
2022-11-19 14:31:06 +0530

    Candidates changes

--- extract.php
+++ extract.php
@@ -90,7 +90,7 @@ function getSkillFromContent($content) {
 }
 function getExperienceFromContent($content) {
     $yearIndex = strpos(strtolower($content), 'year');
-    return (int) substr($content, $yearIndex -3, 3);
+    return (int) substr($content, $yearIndex -4, 4);
 }
 function getPhoneFromContent($content) {
     preg_match_all("/[+91\s-]+[6-9][0-9]{9}/", $content, $phones);
