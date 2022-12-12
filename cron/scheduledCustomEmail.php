9a1d06f1d937114864959e6e83ffb03304fc57b5
Ninad Ramade <ninad.ramade@pegasusone.com>
2022-12-12 11:52:42 +0530

    candidate changes

---
+++ cron/scheduledCustomEmail.php
@@ -0,0 +1,118 @@
+<?php 
+include_once 'config.php';
+use PHPMailer\PHPMailer\PHPMailer;
+require_once 'vendor/autoload.php';
+require_once __DIR__ . '/../vendor/phpmailer/phpmailer/src/Exception.php';
+require_once __DIR__ . '/../vendor/phpmailer/phpmailer/src/PHPMailer.php';
+ini_set('max_execution_time', 0);
+ini_set("memory_limit", "-1");
+set_time_limit(0);
+$db = new mysqli(servername, username, password, dbname);
+$date = date('Y-m-d');
+$emailCount = 0;
+$logHandle = fopen('cron/cron.log', 'c');
+while ($emailCount < 5) {
+    $failedEmails = [];
+    $requirements = [];
+    while (count($requirements) < 1) {
+        $sql = "SELECT * FROM vendor_req WHERE reqdate = '" . $date . "' AND Active = 'Y' AND cronStatus = 0";
+        $result = $db->query($sql);
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $requirements[] = $row;
+            }
+            $where = [];
+            foreach($requirements as $eachReq) {
+                $sql = "UPDATE vendor_req SET cronStatus = 1 WHERE vreqid = " . $eachReq['vreqid'];
+                $db->query($sql);
+                $skills = array_values(array_filter(explode(",", $eachReq['skills'])));
+                $locations = array_values(array_filter(explode(",", $eachReq['worklocation'])));
+                foreach($skills as $eachskill) {
+                    $where[] = 'skills like "%,'. $eachskill .',%"';
+                }
+                $sql = "SELECT * FROM candidates WHERE " . implode(" OR ", $where);
+                $result = $db->query($sql);
+                if ($result->num_rows < 1) {
+                    $sql = "UPDATE vendor_req SET cronStatus = 2 WHERE vreqid = " . $eachReq['vreqid'];
+                    $db->query($sql);
+                    continue;
+                } else {
+                    $candidates = [];
+                    while($row = $result->fetch_assoc()) {
+                        $candidates[] = $row;
+                    }
+                    foreach ($candidates as $candidate) {
+                        if(!empty($candidate['resume'])) {
+                            $sql = "UPDATE vendor_req SET cronStatus = 2 WHERE vreqid = " . $eachReq['vreqid'];
+                            $db->query($sql);
+                            continue;
+                        }
+                        $resultSkills = array_filter(explode(",", $candidate['skills']));
+                        $sql = "SELECT * FROM skills WHERE id IN (" . implode(",", array_unique($resultSkills)) . ")";
+                        try {
+                            $result = $db->query($sql);
+                            $candidateSkills = [];
+                            while($row = $result->fetch_assoc()) {
+                                $candidateSkills[$row['id']] = $row['skill'];
+                            }
+                        } catch (mysqli_sql_exception $e) {
+                        }
+                        $sql = "SELECT * FROM locations WHERE id IN (" . implode(",", array_unique($locations)) . ")";
+                        try {
+                            $result = $db->query($sql);
+                            $candidateLocations = [];
+                            while($row = $result->fetch_assoc()) {
+                                $candidateLocations[$row['id']] = $row['location'];
+                            }
+                        } catch (mysqli_sql_exception $e) {
+                        }
+                        $subject = "Profile for " . (!empty($eachReq['skills']) ? implode(", ", array_intersect_key($candidateSkills, array_flip($skills))) : implode(", ", $candidateSkills));
+                        if(!empty($eachReq['overallexp'])) {
+                            $subject .= " with " . $eachReq['overallexp'] . " Years experience";
+                        }
+                        if(!empty($eachReq['worklocation'])) {
+                            $subject .= " at " . implode(", ", $candidateLocations) . " location";
+                        }
+                        $sql = "INSERT INTO applications (vendorId, candidateId, email, emailSentBy, emailSentOn, subject, status) VALUES (" . $eachReq['vendorid'] . ", " . $candidate['id'] . ", '" . $candidate['email'] . "', 1, '" . date('Y-m-d H:i:s') . "', '" . $subject . "', 'Email sent')";
+                        if($db->query($sql) === TRUE) {
+                            $customEmailResponse = sendCustomEmail($candidate['email'], $candidate['name'], $db->insert_id, $subject, '');
+                            if($customEmailResponse !== true) {
+                                $failedEmails[] = $candidate['email'] . ' Error: ' . $customEmailResponse;
+                            } else {
+                                $emailCount++;
+                            }
+                        }
+                    }
+                }
+                $sql = "UPDATE vendor_req SET cronStatus = 2 WHERE vreqid = " . $eachReq['vreqid'];
+                $db->query($sql);
+            }
+        }
+        $date = date('Y-m-d', strtotime($date . ' - 1 Day'));
+    }
+    $prefix = "Timestamp: " . date('Y-m-d H:i:s') . " - Req Date: ". $date ." - ";
+    if(!empty($failedEmails)) {
+        $log = "Could not send custom email to " . implode(", ", $failedEmails);
+    } else {
+        $log = "All custom emails sent successfully.";
+    }
+    fwrite($logHandle, $prefix . $log . "\r\n");
+}
+function sendCustomEmail($email, $name, $applicationId, $subject, $body) {
+    $protocol = isset($_SERVER['HTTPS']) ? 'https' : 'http';
+    $fromEmail = 'support@rapidjobs.co.in';
+    $mail = new PHPMailer();
+    $mail->setFrom($fromEmail, "RTJobs");
+    $mail->AddAddress($email, $name);
+    $mail->AddReplyTo($fromEmail, "RTJobs");
+    $mail->IsHTML(true);
+    $mail->Subject = $subject;
+    $content = 'Hi, ' . $name . ',<br/><br/>' . $body . '<br/><br/><a href="' . $protocol . '://profiles.rapidjobs.co.in' . baseurl . 'apply.php?id=' . base64_encode($applicationId) . '" target="blank">Apply Now</a><br/><br/>Thanks<br/><br/>RT Jobs';
+    $mail->Body = $content;
+    if(!$mail->Send()) {
+        return $mail->ErrorInfo;
+    }
+    return true;
+}
+
+?>
